ecf2601316c050c7eabfabdae60127ee924c6b5d
omejedestiny <omejedestiny10@gmail.com>
2023-09-25 19:55:21 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,7 +4,7 @@
  * @s: input string
  * Return: string in reverse
  */
-void rev_string(char *s);
+void rev_string(char *s)
 {
 	char rev = s[0];
 	int counter = 0;
@@ -18,5 +18,5 @@ void rev_string(char *s);
 		rev = s[i];
 		s[i] = s[counter];
 		s[counter] = rev;
+	}
 }
-
